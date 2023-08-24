917b56110ee01f926195fa3856d432246cbe53ca
Tshepo <tshepo.dev.riba@gmail.com>
2023-08-24 23:21:44 +0200

    updating files after testing

---
+++ ansible/patch-services.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Patching services in the 'monitoring' namespace
+kubectl --namespace monitoring patch svc prometheus-k8s -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl --namespace monitoring patch svc alertmanager-main -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl --namespace monitoring patch svc grafana -p '{"spec": {"type": "LoadBalancer"}}'
+
+# Patching service in the 'kubernetes-dashboard' namespace
+# kubectl --namespace kubernetes-dashboard patch svc kubernetes-dashboard -p '{"spec": {"type": "LoadBalancer"}}'
